8b8883fb92e205f7e318ac3bb21a3e3edd8d94cd
yawhide <ert.mcscrad@gmail.com>
2015-06-02 16:48:17 -0400

    reverted deletedb.sh

--- deletedb.sh
+++ deletedb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rm db.sqlite3
 rm vendors/migrations/0001_initial.p*
-python3 manage.py sqlflush | python3 manage.py dbshell
-python3 manage.py makemigrations vendors
-python3 manage.py sqlmigrate vendors 0001
-python3 manage.py migrate
-python3 manage.py shell < create_test_entries.txt
+python manage.py sqlflush | python manage.py dbshell
+python manage.py makemigrations vendors
+python manage.py sqlmigrate vendors 0001
+python manage.py migrate
+python manage.py shell < create_test_entries.txt
